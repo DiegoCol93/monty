037764eb6ebe31c270f9349d062081ff99389a2c
Diego Lopez <dfelo93@gmail.com>
2020-12-20 00:06:05 -0500

    Adds the top two elements on the stack.

---
+++ added.c
@@ -0,0 +1,38 @@
+#include "monty.h"
+/**
+ * added - Adds the top two elements on the stack and
+ *       pops the top element.
+ *
+ *   Arguments:
+ *     @head:    - Pointer head of the list.
+ *    @line_n:   - Command line.
+ *
+ *      _____   __  ______  ______  ______
+ *     /\  __-./\ \/\  ___\/\  ___\/\  __ \
+ *     \ \ \/\ \ \ \ \  __\\ \ \__ \ \ \/\ \
+ *      \ \____-\ \_\ \_____\ \_____\ \_____\
+ *       \/____/ \/_/\/_____/\/_____/\/_____/
+ *      __      ______  ______  ______  ______
+ *     /\ \    /\  __ \/\  == \/\  ___\/\___  \
+ *     \ \ \___\ \ \/\ \ \  _-/\ \  __\\/_/  /__
+ *      \ \_____\ \_____\ \_\   \ \_____\/\_____\
+ *       \/_____/\/_____/\/_/    \/_____/\/_____/
+ *
+ *                     Dec-2020
+ */
+void added(stack_t **head, unsigned int line_n)
+{
+	stack_t *top = NULL;
+
+	top = *head;
+	if (!top || !top->next)
+	{
+		fprintf(stderr,
+			"\033[31mL%d: can't add, stack too short\033[0m\n",
+			line_n);
+		exit(EXIT_FAILURE);
+	}
+	top->next->n = top->n + top->next->n;
+	*head = top->next;
+	free(top);
+}
