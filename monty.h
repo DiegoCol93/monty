2a5272eadc10b9d2e2bbd588f5380addba198bbb
Diego Lopez <dfelo93@gmail.com>
2020-12-19 21:19:06 -0500

    Updated comments.

--- monty.h
+++ monty.h
@@ -10,15 +10,17 @@
 /*                               Structures.                                 */
 /*---------------------------------------------------------------------------*/
 
+/* - - - - - - - Strcuct 1.                                                  */
 /**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
+ * struct stack_s - Doubly linked list representation
+ *                  of a stack (or queue).
+ *    Elements:
+ *       @n:      - Integer.
+ *      @prev:    - Points to the previous element of the stack (or queue).
+ *      @next:    - Points to the next element of the stack (or queue).
  *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
- * | - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
+ *   Description: - Doubly linked list node structure for stack, queues,
+ *                  LIFO, FIFO Holberton project.
  */
 typedef struct stack_s
 {
@@ -26,14 +28,18 @@ typedef struct stack_s
 	struct stack_s *prev;
 	struct stack_s *next;
 } stack_t;
+
+/* - - - - - - - Strcuct 2.                                                  */
 /**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
+ * struct instruction_s - Opcode and its function.
  *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
- * | - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
+ *      Elements:
+ *       @opcode:       - The opcode.
+ *          @f:         - Function to handle the opcode.
+ *
+ *     Description:     - Opcode and its function
+ *                        for stack, queues,
+ *                        LIFO, FIFO Holberton project.
  */
 typedef struct instruction_s
 {
